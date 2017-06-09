2961a72f2240ba86709035cad60701c489e73afd
Poetry <admin@cnbbx.com>
2017-06-09 15:41:08 +0800

    新增限制几代提交数据； 历史记录显示几代；

--- src/components/history.vue
+++ src/components/history.vue
@@ -1,42 +1,44 @@
 <template>
   <el-table :data="tableData" :default-sort="{prop: 'date', order: 'descending'}">
-    <el-table-column prop="commodity_time" label="日期" sortable>
-    </el-table-column>
-    <el-table-column prop="boutique_name" label="货品名" sortable>
-    </el-table-column>
-    <el-table-column prop="nums" label="数量" :formatter="formatter">
-    </el-table-column>
+    <el-table-column prop="username" label="姓名"></el-table-column>
+    <el-table-column prop="cft" label="第几代" :formatter="cft_formatter"></el-table-column>
+    <el-table-column prop="commodity_time" label="日期" sortable></el-table-column>
+    <el-table-column prop="boutique_name" label="货品名"></el-table-column>
+    <el-table-column prop="nums" label="数量" :formatter="formatter"></el-table-column>
   </el-table>
 </template>
 
 <script>
-export default {
-  data: function () {
-    return {
-      tableData: []
-    }
-  },
-  mounted: function () {
-    var _this = this;
-    this.Ajax.post('history', this.Qs.stringify({
-      mobile: _this.Cookie.get('mobile')
-    }))
-      .then(function (response) {
-        var result = response.data;
-        if (result.code == 1) {
-          _this.tableData = result.datas;
-        } else {
-          _this.tableData = [];
+    export default {
+        data: function() {
+            return {
+                tableData: []
+            }
+        },
+        mounted: function() {
+            var _this = this;
+            this.Ajax.post('history', this.Qs.stringify({
+                    mobile: _this.Cookie.get('mobile')
+                }))
+                .then(function(response) {
+                    var result = response.data;
+                    if (result.code == 1) {
+                        _this.tableData = result.datas;
+                    } else {
+                        _this.tableData = [];
+                    }
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        },
+        methods: {
+            formatter: function(row, column) {
+                return row.buy_num + row.unit;
+            },
+            cft_formatter: function(row, column) {
+                return row.cft + "代";
+            },
         }
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  },
-  methods: {
-    formatter: function (row, column) {
-      return row.buy_num + row.unit;
     }
-  }
-}
 </script>
